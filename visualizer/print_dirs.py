1cb5952262071e9ec6562e47cec14e36dae06165
hhpfiona <128571507+hhpfiona@users.noreply.github.com>
2023-09-10 13:04:21 -0400

    Add files via upload

---
+++ visualizer/print_dirs.py
@@ -0,0 +1,38 @@
+import os
+from tm_trees import ordered_listdir
+
+
+def print_items(d: str = os.path.join(".", "example-directory")
+                , indentation: str = '') -> None:
+    """
+    A sample program showing how to recurse on directories using os.
+
+    Print the list of files and directories in directory <d>, recursively,
+    prefixing each with the given <indentation>.
+    """
+    for filename in ordered_listdir(d):
+        subitem = os.path.join(d, filename)
+        if os.path.isdir(subitem):
+            print(indentation + filename + os.sep)
+            print_items(subitem, indentation + '  ')
+        else:
+            print(indentation + filename)
+
+
+if __name__ == '__main__':
+    # Put in a path for a directory, like
+    # 'C:\\Users\\David\\Documents\\csc148\\assignments' (Windows) or
+    # '/Users/dianeh/Documents/courses/csc148/assignments' (OSX)
+    # to print the contents of that directory.
+    # Tips:
+    # . means the current directory
+    # .. means the parent directory of the current directory
+    # these can be useful for specifying relative paths and aren't
+    # dependent on the full path, which is specific to you and your
+    # file structure on your computer.
+    # As a default, we've specified PATH to be for the example
+    # directory provided in the starter code zip file.
+    PATH = os.path.join(".", "example-directory")
+    print(f"calling print_items on path: {PATH}, "
+          f"with base name: {os.path.basename(PATH)}")
+    print_items(PATH)
